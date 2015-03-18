4ada9282bbfa8c9e07f6c4924a1f57fdf92e7855
Nicholas Fish <nicholas.fish@fastwebmedia.com>
2015-03-18 15:49:05 +0000

    temp models name git workaround

---
+++ src/tempModels/Campaign.php
@@ -0,0 +1,21 @@
+<?php namespace Fastwebmedia\LaravelVouchering\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Campaign extends Model
+{
+    protected $table = 'voucher_campaigns';
+    protected $fillable = [
+        'name',
+        'brand',
+        'urn',
+        'starts_at',
+        'ends_at',
+        'is_active',
+    ];
+
+    public function vouchers()
+    {
+        return $this->hasMany('Vouchers\Voucher', 'entry_id', 'id');
+    }
+}
